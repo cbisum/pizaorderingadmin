f73d6799ded20f3d33c3dad25a06122ce5ac6cdb
CB <cbshrestha24@gmail.com>
2022-05-27 19:02:42 +1000

    Initial Commit

---
+++ demo/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+
+
+
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+
+
+</body>
+</html>
---
+++ practice/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> practice</title>
+</head>
+
+<body>
+
+<?php
+
+
+
+?>
+
+</body>
+</html>
